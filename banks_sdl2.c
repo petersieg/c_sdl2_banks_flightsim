833049003b3999e7a453e41a73b713af699fcdf7
Peter Sieg <16632721+petersieg@users.noreply.github.com>
2026-03-05 10:08:35 +0100

    Add files via upload

--- banks_sdl2.c
+++ banks_sdl2.c
@@ -1,3 +1,80 @@
+/* De-obfuscating the Banks flight simulator from IOCCC 1998. Not finished,
+I haven’t tracked down all the math though author has mentioned he used
+orthogonal matrices.
+
+Stdin is lists of 3d point coordinates. Each list is an object
+drawn connect-a-dot and terminated with 0 0 0. Z upward is negative.
+
+Get the .scene data files from the IOCCC site.
+
+http://www0.us.ioccc.org/years.html#1998
+
+Controls:
+
+Arrow keys are the flight stick.
+Enter re-centers stick left-right, but not forward-back.
+PageUp, PageDn = throttle
+
+HUD on bottom-left:
+speed, heading (0 = North), altitude
+
+Math note:
+
+Angles
+There are 3 angles in the math. Your compass heading, your
+front-back tilt, and your sideways tilt = Tait-Bryan
+angles typical in aerospace math. Also called yaw, pitch, and roll.
+The Z axis is negative upward. That’s called left-handed coordinates.
+The rotation matrix assumes that.
+
+The rotation matrix is not shown in final form in the wiki article I cite,
+so let’s derive it:
+
+cx = cos(x) and so on. x=sideTilt, y=forwardTilt, z=compass
+
+1 0 0 cy 0 -sy cz sz 0
+
+0 cx sx * 0 1 0 * -sz cz 0
+
+0 -sx cx sy 0 cy 0 0 1
+
+cy 0 -sy cz sz 0
+
+sx*sy cx sx*cy * -sz cz 0
+
+cx*sy -sx cx*cy 0 0 1
+
+cy*cz cy*sz -sy
+
+sx*sy*cz-cx*sz sx*sy*sz+cx*cz sx*cy
+
+cx*sy*cz+sx*sz cx*sy*sz-sx*cz cx*cy
+
+
+It is shown in section 2.2.1 “Euler Angles” of banks1
+references
+
+wiki1 = en.wikipedia.org/wiki/Perspective_transform#Perspective_projection
+wiki2 = https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix
+banks1 = “A DISCUSSION OF METHODS OF REAL-TIME AIRPLANE FLIGHT SIMULATION”
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.510.7499&rep=rep1&type=pdf
+
+Obfuscation notes.
+
+The original program has some tricky syntax.
+
+Forms like –*(DN -N? N-DT ?N== RT?&u: & W:&h:&J )
+are nested x ? y :z that return the address of a variable that
+gets de-referenced by the * and finally decremented.
+
+A comma like in c += (I = M / l, l * H + I * M + a * X) * _;
+
+makes a sequence of statements. The last one is the value of the
+
+(…) list.
+
+*/
+
 /*
  * SDL2 port of Banks Flight Simulator (IOCCC 1998)
  * Original math, projection, stdin scene, and physics preserved
@@ -125,9 +202,9 @@ void updateInfoString() {
 
 /* --- Update display --- */
 void updateDisplay() {
-    SDL_SetRenderDrawColor(renderer, 0,0,0,255);
-    SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0,0,0,255);
 
     for (idx = 0, prevX = 1E4; idx < num_pts; ++idx) {
         shiftWorldObjectVertex(idx);
